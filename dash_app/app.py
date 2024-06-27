0eeeea043c1d6e45fd7490d11ed1a5730066ff49
kz-2000 <131752906+kz-2000@users.noreply.github.com>
2024-06-27 14:44:28 +0200

    flask

--- dash_app/app.py
+++ dash_app/app.py
@@ -3,13 +3,13 @@ import dash_bootstrap_components as dbc
 from layout.layout import create_layout
 from callbacks.callbacks import register_callbacks
 from dash_bootstrap_templates import load_figure_template
+from flask import Flask
 
 
 # Makes the Bootstrap Themed Plotly templates available
 load_figure_template('vapor_dark')
 
-# Initialize server
-server = app.server
+server = Flask(__name__)
 
 # Initialize the Dash app with the Quartz Bootstrap theme and Google Fonts
 app = dash.Dash(__name__, external_stylesheets=[
